2da4be462f1a9979ed2b4032b29b1e4831a5ccf7
Nico Baier <nico.baier@gmail.com>
2025-06-07 16:38:07 -0500

    initial commit

---
+++ alchemy.run.ts
@@ -0,0 +1,19 @@
+/// <reference types="node" />
+
+import alchemy from "alchemy";
+import { Vite } from "alchemy/cloudflare";
+
+const app = await alchemy("cloudflare-vite", {
+	phase: process.argv.includes("--destroy") ? "destroy" : "up",
+});
+
+export const website = await Vite("vite-website", {
+	main: "./worker/index.ts",
+	command: "bun run build",
+});
+
+console.log({
+	url: website.url,
+});
+
+await app.finalize();
